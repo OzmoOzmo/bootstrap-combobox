899668c502099706c0cc75ead628d9f905d8afb7
OzmoOzmo <ozmo.spam@gmail.com>
2017-03-01 15:46:07 +0000

    Regex error Fix. Generating too much HTML.
    Function was generating code like
    <strong>a</strong><strong>b</strong><strong>c</strong>...
    this fix makes it generate 
    <strong>abc</strong>
    Was causing IE and IPhone to become unresponsive when more than 800 items in the dropdown.

--- js/bootstrap-combobox.js
+++ js/bootstrap-combobox.js
@@ -205,11 +205,11 @@
 
   , highlighter: function (item) {
       var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
-      return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
-        return '<strong>' + match + '</strong>';
+      var res = item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
+            return match;
       })
-    }
-
+      return '<strong>' + res + '</strong>';
+  }
   , render: function (items) {
       var that = this;
 
